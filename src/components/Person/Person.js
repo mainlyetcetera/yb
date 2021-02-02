7ced8eba203bd8a5ca29e5ef46e09b972ae9b318
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-01 22:01:17 -0700

    Add photos

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Person.css';
 
-const Person = ({id, name, quote, superlative}) => (
+const Person = ({id, name, quote, superlative, photo}) => (
   <div className='person'>
     <h3>{id}</h3>
     <h3>{name}</h3>
     <h3>{quote}</h3>
     <h3>{superlative}</h3>
+    <img src={photo} alt={name}></img> 
   </div>
 )
    
